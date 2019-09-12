ae874b321513d2fef145d30f11fbe9d601d9e043
Ricardo <Ricardocrespo@Gmail.com>
2019-09-11 21:48:50 -0500

    Initial Commit

---
+++ src/test/java/com/tdd/stack/StackApplicationTests.java
@@ -0,0 +1,21 @@
+package com.tdd.stack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class StackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
