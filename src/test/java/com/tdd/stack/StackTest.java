1848a266147b09c95f7001311a77cad67dc3c3d3
Ricardo <Ricardocrespo@Gmail.com>
2019-09-11 21:43:52 -0500

    Initial Commit

---
+++ src/test/java/com/tdd/stack/StackTest.java
@@ -0,0 +1,11 @@
+package com.tdd.stack;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StackTest {
+
+}
